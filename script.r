47305ed5b0553064ac25c3083df398ee508c6721
spencerrjenkins <srjnk01@gmail.com>
2025-05-01 18:28:31 -0400

    Use the provided script to import our data to the R studio program

---
+++ script.r
@@ -0,0 +1,22 @@
+library(Matrix)
+# Read the matrix (replace with the correct path to matrix file)
+expr_mat <- readMM("../scRNA_Compression/data/GSE294399_WBC_020823_matrix.mtx/matrix.mtx")
+
+# Read gene and cell names (replace with the correct paths to gene and cell files)
+genes <- read.table("../scRNA_Compression/data/GSE294399_WBC_020823_features.tsv/features.tsv", stringsAsFactors = FALSE)[,1]
+cells <- read.table("../scRNA_Compression/data/GSE294399_WBC_020823_barcodes.tsv/barcodes.tsv", stringsAsFactors = FALSE)[,1]
+
+# Assign row and column names
+rownames(expr_mat) <- genes
+colnames(expr_mat) <- cells
+
+library(RaceID)
+sc <- SCseq(as.matrix(expr_mat))
+sc <- filterdata(sc,mintotal=2000)
+sc <- compdist(sc,metric="pearson")
+sc <- clustexp(sc)
+sc <- findoutliers(sc)
+sc <- rfcorrect(sc)
+sc <- comptsne(sc)
+sc <- compfr(sc)
+plotmap(sc)
