397e5198b879e7c39bdfca3432a1261c8897ad8f
Niko Zhang <hiniko23@gmail.com>
2025-05-15 10:22:42 -0400

    counts file added to compressor

--- compressor.py
+++ compressor.py
@@ -3,7 +3,7 @@ from collections import defaultdict
 import csv
 import numpy as np
 
-def compress(cluster_assignments_file, cluster_out_file, deltas_file, matrix):
+def compress(cluster_assignments_file, cluster_out_file, deltas_file, counts_file, matrix):
     # Load the pickled array from the file
     extension = cluster_assignments_file.split('.')[-1]
     if extension == 'pkl':
@@ -28,23 +28,31 @@ def compress(cluster_assignments_file, cluster_out_file, deltas_file, matrix):
             cluster_genes[i] = set.intersection(*genes[i])
             
     deltas = defaultdict(lambda: set())
+    counts = []
     for i in range(matrix.shape[1]):# Cell
         genes_set = set(np.arange(0, matrix.shape[0] + 0)[matrix[:,i].astype(bool)].tolist())
         deltas[i] = genes_set - cluster_genes[cluster_labels[i]]
+        counts.append(matrix[:,i][matrix[:,i] > 0].tolist())
         
     with open(cluster_out_file, 'w', newline='') as file:
         writer = csv.writer(file)
         for k in cluster_genes:
-            writer.writerow([k] + list(cluster_genes[k]))
+            writer.writerow(list(cluster_genes[k]))
 
     with open(deltas_file, 'w', newline='') as file:
         writer = csv.writer(file)
         for d in deltas:
             writer.writerow([cluster_labels[d]] + list(deltas[d]))
 
-def decompress(clusters_file, deltas_file, shape):
+    with open(counts_file, 'w', newline='') as file:
+        writer = csv.writer(file)
+        for c in counts:
+            writer.writerow(c)
+
+def decompress(clusters_file, deltas_file, counts_file, shape):
     gene_map = defaultdict(lambda: [None, set()])
     cluster_map = defaultdict(lambda: set())
+    counts = []
     with open(deltas_file, newline='') as f:
         reader = csv.reader(f)
         for i, row in enumerate(reader):
@@ -56,15 +64,19 @@ def decompress(clusters_file, deltas_file, shape):
 
     with open(clusters_file, newline='') as f:
         reader = csv.reader(f)
-        for row in reader:
-            cluster = int(row[0])
-            genes = set(map(int, row[1:]))
+        for cluster, row in enumerate(reader):
+            genes = set(map(int, row))
             for cell in cluster_map[cluster]:
                 gene_map[cell][1] = gene_map[cell][1].union(genes)
 
+    with open(counts_file, newline='') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            counts.append(row)
+
     cell_gene_matrix = np.zeros(shape)
     for cell_idx in range(0, num_cells+1):
-        for g in gene_map[cell_idx][1]:
-            cell_gene_matrix[g, cell_idx] = 1
+        for i, g in enumerate(sorted(gene_map[cell_idx][1])):
+            cell_gene_matrix[g, cell_idx] = counts[cell_idx][i]
     
     return cell_gene_matrix
