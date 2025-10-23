40c70f9d96589846d97f3158940972b4bb014efc
Niko Zhang <hiniko23@gmail.com>
2025-10-22 21:23:31 -0400

    extended delta compression

--- compressor.py
+++ compressor.py
@@ -6,6 +6,12 @@ from scipy.io import mmread
 import heapq
 import collections
 import os
+import shutil
+
+#fpgrowth libraries
+from mlxtend.preprocessing import TransactionEncoder
+from mlxtend.frequent_patterns import fpgrowth
+import pandas as pd
 
 
 # Huffman code retrieved from https://www.geeksforgeeks.org/huffman-coding-in-python/
@@ -54,6 +60,24 @@ def print_tree(node, prefix="", is_left=True):
         if node.left or node.right:
             print_tree(node.left, prefix + ("│   " if is_left else "    "), True)
             print_tree(node.right, prefix + ("│   " if is_left else "    "), False)
+
+# helper copy file function
+def copy_file(source_path, destination_path):
+    """Copies a file from the source path to the destination path.
+
+    Args:
+        source_path: The path to the file to be copied.
+        destination_path: The path to the destination directory.
+    """
+    try:
+        shutil.copy(source_path, destination_path)
+        print(f"File copied successfully from {source_path} to {destination_path}")
+    except FileNotFoundError:
+        print(f"Error: Source file not found: {source_path}")
+    except PermissionError:
+        print(f"Error: Permission denied to access {source_path} or {destination_path}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
 
 def compress(cluster_assignments_file, matrix_path, out_path):
     # Load the pickled array from the file
@@ -104,77 +128,193 @@ def compress(cluster_assignments_file, matrix_path, out_path):
         for c in counts:
             writer.writerow(c)
 
-    def low_level_compress(target):
-        with open(out_path + '/high_level_compress/' + target + '.csv', newline='') as f:
-            lines = f.readlines()
+    # Extended compression of deltas
+    if not os.path.isdir(out_path + '/high_level_compress2'):
+        os.mkdir(out_path + '/high_level_compress2')
+    copy_file(out_path + '/high_level_compress/cluster_genes.csv', out_path + '/high_level_compress2')
+    copy_file(out_path + '/high_level_compress/counts.csv', out_path + '/high_level_compress2')
 
-            content = []
-            for l in lines:
-                content += list(map(int, l.replace('\r', '').replace('\n', '').split(',')))
-                content.append('\n')
-            content = content[:-1]
+    # compress deltas further
+    def compress_high_level_deltas(in_path):
+        with open(in_path + '/high_level_compress/deltas.csv', newline='') as f:
+            reader = csv.reader(f)
+            dataset = []
+            cluster_labels = []
+            for i, row in enumerate(reader):
+                dataset.append(row[1:])
+                cluster_labels.append(row[0])
+            f.close()
 
-            root = build_huffman_tree(collections.Counter(content))
-            huffman_codes = generate_huffman_codes(root)
-
-            # Pickle the array and save to a file
-            if not os.path.isdir(out_path + '/low_level_compress'):
-                os.mkdir(out_path + '/low_level_compress')
-            filename = out_path + '/low_level_compress/' + target + '_huffman_tree'
-            with open(filename, 'wb') as file:
-                pickle.dump((root, len(content)), file)
-            file.close()
+        chosens = []
+        flags = []
+        support = 0.95
+        iter = 1
+        while len(chosens) < 2000:
+            te = TransactionEncoder()
+            te_ary = te.fit(dataset).transform(dataset)
+            df = pd.DataFrame(te_ary, columns=te.columns_)
             
-            with open(out_path + '/low_level_compress/huffman_encoded_'+target, 'wb') as file:
-                leftover = ''
-                for index, l in enumerate(lines):
-                    elems = l.replace('\r', '').replace('\n', '').split(',')
-                    elems = list(map(int, elems))
-                    if index < len(lines) - 1:
-                        elems.append('\n')
-                    binary_string = leftover
-                    for e in elems:
-                        binary_string += huffman_codes[e]
+            if support < 1:
+                df_iter = df.sample(n = 2, random_state=iter)
+            else:
+                df_iter = df
 
-                    main_part = binary_string[:(8 * (len(binary_string) // 8))]
-                    leftover = binary_string[(8 * (len(binary_string) // 8)):]
+            s = fpgrowth(df_iter, min_support=support, use_colnames=True, max_len = 2)
 
-                    for i in range(0, len(main_part), 8):
-                        byte_string = main_part[i:i + 8]
-                        byte_value = int(byte_string, 2)
-                        file.write(bytes([byte_value]))
+            mlength = 1
+            chosen = []
+            choice = None
+            for ele in s['itemsets']:
+                if len(ele) > mlength:
+                    mlength = len(ele)
+                    choice = set(ele)
+            if choice == None:
+                support -= 0.01
+                support = round(support, 2)
+                iter += 1
+                continue
+            chosen.append(choice)
+            temp_set = choice
+            for ele in s['itemsets']:
+                ele_set = set(ele)
+                if len(ele) > 1 and not temp_set.intersection(ele_set):
+                    temp_set = temp_set.union(ele_set)
+                    chosen.append(ele_set)
+            if len(chosen) < 50:
+                support -= 0.01
+                support = round(support, 2)
+            chosens = chosens + chosen
 
-                if leftover:
-                    leftover = leftover.ljust(8, '0')
-                    byte_value = int(leftover, 2)
-                    file.write(bytes([byte_value]))
+            deltas = [set(x) for x in dataset]
+            flag = []
+            for c in chosen:
+                f = set()
+                for i, d in enumerate(deltas):
+                    if c.intersection(d) == c:
+                        deltas[i] = (d - c)
+                        f.add(i)
+                flag.append(f)
+            flags += flag
+            print('support: ', support)
+            print('chosens: ', len(chosens))
+            iter += 1
+            dataset = [list(x) for x in deltas]
+        
+        with open(in_path + '/high_level_compress2/deltas.csv', 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([len(chosens)]) # number of common item sets
+            for chosen in chosens:
+                writer.writerow(list(chosen)) # write current commom item set
+            for i, d in enumerate(deltas):
+                row = [cluster_labels[i]]
+                for j, flag in enumerate(flags):
+                    if i in flag:
+                        row += ['#'+str(j)]
+                row += list(d)
+                writer.writerow(row)
             file.close()
-        f.close()
 
-    low_level_compress('deltas')
-    low_level_compress('counts')
+    compress_high_level_deltas(out_path)
+
+    def low_level_compress(folder_num):
+        for file_name in os.listdir(out_path + '/high_level_compress'+folder_num):
+            if file_name != 'cluster_genes.csv':
+                with open(out_path + '/high_level_compress'+folder_num+'/' + file_name, newline='') as f:
+                    lines = f.readlines()
+
+                    content = []
+                    for l in lines:
+                        content += list(map(str, l.replace('\r', '').replace('\n', '').split(',')))
+                        content.append('\n')
+                    content = content[:-1]
+
+                    root = build_huffman_tree(collections.Counter(content))
+                    huffman_codes = generate_huffman_codes(root)
+
+                    # Pickle the array and save to a file
+                    if not os.path.isdir(out_path + '/low_level_compress'+folder_num):
+                        os.mkdir(out_path + '/low_level_compress'+folder_num)
+                    filename = out_path + '/low_level_compress'+folder_num+'/' + file_name.replace('.csv', '') + '_huffman_tree'
+                    with open(filename, 'wb') as file:
+                        pickle.dump((root, len(content)), file)
+                    file.close()
+                    
+                    with open(out_path + '/low_level_compress'+folder_num+'/huffman_encoded_'+file_name.replace('.csv', ''), 'wb') as file:
+                        leftover = ''
+                        for index, l in enumerate(lines):
+                            elems = l.replace('\r', '').replace('\n', '').split(',')
+                            elems = list(map(str, elems))
+                            if index < len(lines) - 1:
+                                elems.append('\n')
+                            binary_string = leftover
+                            for e in elems:
+                                binary_string += huffman_codes[e]
+
+                            main_part = binary_string[:(8 * (len(binary_string) // 8))]
+                            leftover = binary_string[(8 * (len(binary_string) // 8)):]
+
+                            for i in range(0, len(main_part), 8):
+                                byte_string = main_part[i:i + 8]
+                                byte_value = int(byte_string, 2)
+                                file.write(bytes([byte_value]))
+
+                        if leftover:
+                            leftover = leftover.ljust(8, '0')
+                            byte_value = int(leftover, 2)
+                            file.write(bytes([byte_value]))
+                    file.close()
+                f.close()
+    
+    low_level_compress('') # low-level compress original implementation
+    low_level_compress('2') # low-level compress extended implementation
 
 def high_level_decompress(in_path, matrix_path):
     gene_map = defaultdict(lambda: [None, set()])
     cluster_map = defaultdict(lambda: set())
     counts = []
-    with open(in_path + '/high_level_compress/deltas.csv', newline='') as f:
+    with open(in_path + '/deltas.csv', newline='') as f:
         reader = csv.reader(f)
-        for i, row in enumerate(reader):
-            cluster = int(row[0])
-            genes = set(map(int, row[1:])) if len(row) > 1 else set()
-            gene_map[i] = [cluster, genes]
-            cluster_map[cluster].add(i)
-            num_cells = i
+        if in_path[-1] == '2':
+            num_item_sets = 0
+            item_sets = []
+            deltas = []
+            for i, row in enumerate(reader):
+                if i == 0:
+                    num_item_sets = int(row[0])
+                elif i <= num_item_sets:
+                    item_sets.append((int(row[0]), int(row[1])))
+                else:
+                    delta = []
+                    for c in row:
+                        if c[0] != '#':
+                            delta.append(int(c))
+                        else:
+                            a, b = item_sets[int(c[1:])]
+                            delta.append(a)
+                            delta.append(b)
+                    deltas.append(delta)
+            
+            for i, d in enumerate(deltas):
+                gene_map[i] = [d[0], set(d[1:])]
+                cluster_map[d[0]].add(i)
+                num_cells = i            
+        else:
+            for i, row in enumerate(reader):
+                cluster = int(row[0])
+                genes = set(map(int, row[1:])) if len(row) > 1 else set()
+                gene_map[i] = [cluster, genes]
+                cluster_map[cluster].add(i)
+                num_cells = i
+        f.close()
 
-    with open(in_path + '/high_level_compress/cluster_genes.csv', newline='') as f:
+    with open(in_path + '/cluster_genes.csv', newline='') as f:
         reader = csv.reader(f)
         for cluster, row in enumerate(reader):
             genes = set(map(int, row))
             for cell in cluster_map[cluster]:
                 gene_map[cell][1] = gene_map[cell][1].union(genes)
 
-    with open(in_path + '/high_level_compress/counts.csv', newline='') as f:
+    with open(in_path + '/counts.csv', newline='') as f:
         reader = csv.reader(f)
         for row in reader:
             counts.append(row)
@@ -185,18 +325,18 @@ def high_level_decompress(in_path, matrix_path):
         for i, g in enumerate(sorted(gene_map[cell_idx][1])):
             cell_gene_matrix[g, cell_idx] = counts[cell_idx][i]
     
-    print("High Level Accuracy Check " + "Passed" if (cell_gene_matrix != matrix).sum(axis=None) == 0 else "Failed")
+    print("High Level Accuracy Check " + "Passed" if (cell_gene_matrix != matrix).sum(axis=None) == 0 else "Failed")    
 
-def low_level_decompress(in_path):
+def low_level_decompress(in_path, folder_num):
     def helper_decompress(target):
-        with open(in_path + '/low_level_compress/' + target + '_huffman_tree', 'rb') as file:
+        with open(in_path + '/low_level_compress'+folder_num+'/' + target + '_huffman_tree', 'rb') as file:
             # Load the pickled object from the file
             root, length = pickle.load(file)
         file.close()
 
         decoded_lines = []
         count = 0
-        with open(in_path + '/low_level_compress/huffman_encoded_' + target, 'rb') as file:
+        with open(in_path + '/low_level_compress'+folder_num+'/huffman_encoded_' + target, 'rb') as file:
             decoded_line = []
             cur = root
             n = 0
@@ -223,11 +363,13 @@ def low_level_decompress(in_path):
 
         result = 'ENCODED CORRECTLY!'
         lines = ''
-        with open(in_path + '/high_level_compress/' + target + '.csv', newline='') as f:
+        with open(in_path + '/high_level_compress'+folder_num+'/' + target + '.csv', newline='') as f:
             lines = f.readlines()
         for i,l in enumerate(lines):
-            l = list(map(int, l.split(',')))
+            l = list(l.replace('\r', '').replace('\n', '').split(','))
             if (l != decoded_lines[i]):
+                print(l)
+                print(decoded_lines[i])
                 result = 'ENCODED WRONG!!'
                 break
         print(target + ' huffman : ' + result)
